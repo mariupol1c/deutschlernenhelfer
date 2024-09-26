15371299485dfc4cb01d64cbc1c743450024b3f1
yevhen.kuleshov <yevhen.kuleshov@crossinx.com>
2024-09-26 15:49:32 +0200

    Adding Test to gitignor

--- src/test/java/com/mariupol/deutschlernenhelfer/DeutschlernenhelferApplicationTests.java
+++ src/test/java/com/mariupol/deutschlernenhelfer/DeutschlernenhelferApplicationTests.java
@@ -1,13 +1,3 @@
 package com.mariupol.deutschlernenhelfer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DeutschlernenhelferApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
